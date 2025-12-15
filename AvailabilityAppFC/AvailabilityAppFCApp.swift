5d183486bcf64af06acd01a5ca047764b74af057
slackappu <alexveliyathumalil21@gmail.com>
2025-12-15 08:27:05 -0800

    Initial Commit

---
+++ AvailabilityAppFC/AvailabilityAppFCApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvailabilityAppFCApp.swift
+//  AvailabilityAppFC
+//
+//  Created by Alex J. Veliyathumalil on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct AvailabilityAppFCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AvailabilityAppFC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AvailabilityAppFC
+//
+//  Created by Alex J. Veliyathumalil on 12/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
