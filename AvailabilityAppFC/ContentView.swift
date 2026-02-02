da950c70955449ed6b1e408f386960358240877b
aprajapati50 <aprajapati7330@stu.d214.org>
2026-02-02 10:18:19 -0600

    nothing, just pulling and pushing, deleted commented stuff

--- AvailabilityAppFC/ContentView.swift
+++ AvailabilityAppFC/ContentView.swift
@@ -31,7 +31,7 @@ struct ContentView: View {
                     BDayView()
                 }
                 
-                Spacer() // optional, but helps push content upward
+                Spacer()
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
             
@@ -42,58 +42,6 @@ struct ContentView: View {
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
             .padding()
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        //        ZStack{
-        //            VStack{
-        //                HStack{
-        //                    Button {
-        //                    changeDay(by: -1)
-        //                    } label: {
-        //                        Image(systemName: "arrow.left")
-        //                    }
-        //
-        //                    DatePicker("", selection: $wakeUp, displayedComponents: .date)
-        //                        .labelsHidden()
-        //                    Button {
-        //                        changeDay(by: 1)
-        //                    } label: {
-        //                        Image(systemName: "arrow.right")
-        //                    }
-        //
-        //                }
-        //                // switches data based on whether its an A day or not
-        //                if isADay(for: wakeUp) {
-        //                    ADayView()
-        //                } else {
-        //                    BDayView()
-        //                }
-        //
-        //            }
-        //            VStack{
-        //                Circle()
-        //                    .frame(width: 50, height: 50, alignment: .topTrailing)
-        //                Button {
-        //                    fetchCSVData()
-        //                } label: {
-        //                    Text("test")
-        //                }
-        //
-        //            }
-        //            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
-        //            .padding()
-//    }
-        
         .foregroundStyle(isADay(for: wakeUp) ? .orange : .brown)
         .animation(.easeInOut(duration: 0.25), value: wakeUp)
 
